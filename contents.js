e42b27209f6e9d81ae3fdaeff6c0c42eecc3ff53
Roy Solberg <git@roysolberg.com>
2017-07-28 16:27:59 +0200

    Added blog posts into separate file.

---
+++ contents.js
@@ -0,0 +1,77 @@
+window.SpaBlog = window.SpaBlog || {}; // Our namespace
+
+(function (SpaBlog) {
+    SpaBlog.posts =
+        [
+            {
+                title: 'Bulding a real SPA blog',
+                date: '2017-07-27 22:00',
+                summary: 'This blog is a real SPA with no backend code.',
+                niceUrl: '/2017/07/building-a-real-spa-blog',
+                category:
+                {
+                    title: 'Software development',
+                    url: '/software-development'
+                },
+                tags: [
+                    {
+                        title: 'JavaScript',
+                        url: '/javascript'
+                    }
+                ]
+            },
+            {
+                title: 'Chromecast on the go',
+                niceUrl: '/chromecast-on-the-go',
+                category:
+                {
+                    title: 'Tech',
+                    url: '/tech'
+                },
+                tags: [
+                    {
+                        title: 'Chromecast',
+                        url: '/chromecast'
+                    }
+                ]
+            },
+            {
+                title: 'The pain of releasing an app in 2017',
+                niceUrl: '/the-pain-of-releasing-an-app-in-2017',
+                category:
+                {
+                    title: 'Software development',
+                    url: '/software-development'
+                },
+                tags: [
+                    {
+                        title: 'App',
+                        url: '/app'
+                    },
+                    {
+                        title: 'eSports',
+                        url: '/esports'
+                    }
+                ]
+            },
+            {
+                title: 'Information leak using customer\'s e-mail address',
+                niceUrl: '/the-pain-of-releasing-an-app-in-2017',
+                category:
+                {
+                    title: 'Security',
+                    url: '/security'
+                },
+                tags: [
+                    {
+                        title: 'App',
+                        url: '/app'
+                    },
+                    {
+                        title: 'Chromecast',
+                        url: '/chromecast'
+                    }
+                ]
+            }
+        ];
+}(window.SpaBlog));
